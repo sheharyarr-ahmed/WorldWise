9905892d28091ccf632caedd67758ddd46e47467
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-01-29 15:04:53 +0500

    done with the interaction with the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { useSearchParams } from "react-router-dom";
-import { MapContainer, Marker, TileLayer, Popup, useMap } from "react-leaflet";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
 import { useCities } from "../contexts/CitiesContext";
 
@@ -62,6 +69,7 @@ function Map() {
           </Marker>
         ))}
         <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
@@ -69,6 +77,16 @@ function Map() {
 function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+
   return null;
 }
 
